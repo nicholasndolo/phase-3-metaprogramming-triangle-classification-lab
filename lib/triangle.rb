1cc99f4ae0e7de72a30d53e6dc72a3752b977365
Nicholas <nicholas.nmalinda@gmail.com>
2022-08-21 13:05:10 +0300

    commit tree 58b24a0a730703b3dee13ce30e73425794525e95

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
-class Triangle
-  # write code here
-end
+  class Triangle
+    # write code here
+   attr_reader :side_a, :side_b, :side_c
+   def initialize(side_a, side_b, side_c)
+     @side_a = side_a
+     @side_b = side_b
+     @side_c = side_c
+   end
+ 
+   def kind
+     validate_triangle
+     if side_a == side_b && side_b == side_c
+       :equilateral
+     elsif side_a == side_b || side_b == side_c || side_a == side_c
+       :isosceles
+     else
+       :scalene
+     end
+   end
+ 
+   def validate_triangle
+     valid_triangle = [(side_a + side_b > side_c), (side_a + side_c > side_b), (side_b + side_c > side_a)]
+     [side_a, side_b, side_c].each do |side|
+       valid_triangle << false if side <= 0 
+     raise TriangleError if valid_triangle.include?(false)
+     end
+   end
+ 
+   class TriangleError < StandardError
+ 
+   end
+ 
+ end
+ 
